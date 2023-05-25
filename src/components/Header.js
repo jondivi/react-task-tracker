693cfdf1c39273b4d9dd20d79f4561ee3499f74f
Jon <jonathan.divi@gmail.com>
2023-05-25 16:00:10 -0400

    49:13

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 
 const Header = ({title}) => {
-  return (
-    <header className='header'>
-        <h1>{title}</h1>
-        <button  className='btn'>Add</button>
-    </header>
-  )
+    const onClick = () =>{
+        console.log('Click')
+
+    }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick=
+            {onClick} />
+        </header>
+    )
 }
 
 Header.defaultProps = {
