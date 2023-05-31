62ec7cabc2218ed9446c4a30a4fb8880009fc581
Jon <jonathan.divi@gmail.com>
2023-05-31 13:20:18 -0400

    adding routes

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Copyright &copy; 2023</p>
+        <a href='/about'>About</a>
+      
+    </footer>
+  )
+}
+
+export default Footer
